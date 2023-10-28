b5bb7919b779e4b6f6d74fc2d461bb2562367582
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-10-28 17:13:46 -0500

    update /tasks

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,31 +1,108 @@
 const d = document,
-	$search = d.querySelector('.search-dialog'),
-	$searchContent = d.querySelector('.search-dialog input'),
-	$coursesName = d.querySelectorAll('.course-item-name'),
-	$coursesTeacher = d.querySelectorAll('.course-item-teacher'),
-	$topNavbar = d.querySelector('.top-navbar')
+	$topNavbar = d.querySelector('.top-navbar'),
+	$activities = d.querySelectorAll('.activity-item'),
+	$searchNotFound = d.querySelector('.search-not-found'),
+	$paginationButton = d.querySelector('.pagination-button')
 
 function searchItem(search) {
-	filterBySubject(search.value)
+	search = search.toUpperCase()
+	filterQuery($activities, search)
 }
 
-function filterBySubject(query) {
-	query = query.toUpperCase()
-	$coursesName.forEach((course) => {
-		const $listItem = course.parentElement,
-			$list = $listItem.parentElement,
-			$currentCourse = $list.parentElement
-		let name = course.textContent
-		name = name.split(' ')[0]
-		name = name.toUpperCase()
-		if (!name.includes(query)) {
-			$currentCourse.classList.add('hidden')
+function filterByCategory(filter) {
+	const old = d.querySelector('.filter-option-on')
+	let name = filter.dataset.filter
+	name = name.toUpperCase()
+	console.log(old)
+	if (old) {
+		old.classList.remove('filter-option-on')
+	}
+	if (filter.id.includes('filter')) {
+		filter.classList.add('filter-option-on')
+	} else {
+		const parent = filter.parentElement
+		parent.classList.add('filter-option-on')
+	}
+
+	$activities.forEach(($activity) => {
+		const $category = $activity.querySelector('.activity-item-category'),
+			category = $category.innerText.toUpperCase(),
+			categoryButton = $category.parentElement
+
+		if (category !== name) {
+			$activity.classList.add('hidden')
+			categoryButton.classList.remove('filter-option-on')
 		} else {
-			$currentCourse.classList.remove('hidden')
+			$activity.classList.remove('hidden')
 		}
+		categoryButton.classList.add('filter-option-on')
 	})
 }
 
+function filterQuery(set, query) {
+	let hiddenCounter = 0
+	set.forEach((node) => {
+		const $title = node.querySelector('h4'),
+			$content = node.querySelector('p'),
+			title = $title.innerText.toUpperCase(),
+			content = $content.innerText.toUpperCase()
+
+		if (!title.includes(query) || !content.includes(query)) {
+			node.classList.add('hidden')
+			hiddenCounter++
+		}
+		if (title.includes(query) || content.includes(query)) {
+			node.classList.remove('hidden')
+			hiddenCounter--
+			if (query.length > 0) {
+				highlightText(query, $title)
+				highlightText(query, $content)
+			} else {
+				removeHighlightText($title)
+				removeHighlightText($content)
+			}
+		}
+	})
+	if (hiddenCounter === set.length) {
+		$searchNotFound.classList.remove('hidden')
+		$paginationButton.classList.add('hidden')
+	} else {
+		$searchNotFound.classList.add('hidden')
+		$paginationButton.classList.remove('hidden')
+	}
+}
+
+function resetFilter() {
+	const filtered = d.querySelectorAll('.filter-option-on')
+	filtered.forEach((el) => {
+		el.classList.remove('filter-option-on')
+	})
+	$activities.forEach((el) => {
+		el.classList.remove('hidden')
+	})
+}
+
+function highlightText(text, el) {
+	const old = el.querySelectorAll('.highlight-text')
+	old.forEach((oldElement) => {
+		const parent = oldElement.parentNode
+		const textNode = document.createTextNode(oldElement.textContent)
+		parent.replaceChild(textNode, oldElement)
+	})
+
+	const regex = new RegExp(`${text}`, 'gi')
+	const highlight = '<span class="highlight-text">$&</span>'
+	const replace = el.innerHTML.replace(regex, highlight)
+	el.innerHTML = replace
+}
+
+function removeHighlightText(set) {
+	const setHightlight = set.querySelectorAll('.highlight-text')
+	setHightlight.forEach((highlight) => {
+		const textNode = d.createTextNode(highlight.textContent)
+		set.replaceChild(textNode, highlight)
+	})
+}
 function changeNavBG() {
 	const scrollY = window.scrollY
 	if (scrollY > 20) {
@@ -39,8 +116,19 @@ d.addEventListener('scroll', (e) => {
 	changeNavBG()
 })
 
+d.addEventListener('click', (e) => {
+	if (e.target.closest('#reset-filter')) {
+		resetFilter()
+	}
+	if (e.target.closest('.filter-options li')) {
+		if (e.target.id !== 'reset-filter') {
+			filterByCategory(e.target)
+		}
+	}
+})
+
 d.addEventListener('keyup', (e) => {
 	if (e.target.matches('.search-dialog input')) {
-		searchItem(e.target)
+		searchItem(e.target.value)
 	}
 })
