64faff8e48bc98ef7568e80883a87135a79d6ced
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-10-28 13:03:34 -0500

    update /home

--- scripts/home.js
+++ scripts/home.js
@@ -1,80 +1,34 @@
 const d = document,
 	$search = d.querySelector('.search-dialog'),
 	$searchContent = d.querySelector('.search-dialog input'),
-	$filter = d.querySelector('.filter-dialog'),
 	$coursesName = d.querySelectorAll('.course-item-name'),
-	$coursesTeacher = d.querySelectorAll('.course-item-teacher'),
-	$topNavbar = d.querySelector('.top-navbar')
-
-function toggleSearchDialog(button) {
-	const isSearchOff = $search.classList.contains('hidden')
-	if (isSearchOff) {
-		$search.classList.remove('hidden')
-		button.textContent = 'search_off'
-		$searchContent.focus()
-	} else {
-		$search.classList.add('hidden')
-		button.textContent = 'search'
-	}
-}
-
-function toggleFilterItems() {
-	$filter.classList.toggle('hidden')
-}
+	$topNavbar = d.querySelector('.top-navbar'),
+	$searchNotFound = d.querySelector('.search-not-found')
 
 function searchItem(search) {
-	const type = search.dataset.filterBy,
-		query = search.value
-	if (!type) {
-		filterBySubject(query)
-	} else {
-		filterByTeacher(query)
-	}
-}
-
-function filterBySubject(query) {
-	query = query.toUpperCase()
+	let hiddenCounter = 0
+	search = search.toUpperCase()
 	$coursesName.forEach((course) => {
 		const $listItem = course.parentElement,
 			$list = $listItem.parentElement,
-			$currentCourse = $list.parentElement
+			$currentCourse = $list.parentElement,
+			parent = $currentCourse.parentElement
 		let name = course.textContent
 		name = name.split(' ')[0]
 		name = name.toUpperCase()
-		if (!name.includes(query)) {
-			$currentCourse.classList.add('hidden')
+		if (!name.includes(search)) {
+			parent.classList.add('hidden')
+			hiddenCounter++
 		} else {
-			$currentCourse.classList.remove('hidden')
+			parent.classList.remove('hidden')
+			hiddenCounter--
 		}
 	})
-}
-function filterByTeacher(query) {
-	query = query.toUpperCase()
-	$coursesTeacher.forEach((course) => {
-		const $listItem = course.parentElement,
-			$list = $listItem.parentElement,
-			$currentCourse = $list.parentElement
-		let name = course.textContent
-		name = name.split(' ')[1] + name.split(' ')[2]
-
-		name = name.toUpperCase()
-		if (!name.includes(query)) {
-			$currentCourse.classList.add('hidden')
-		} else {
-			$currentCourse.classList.remove('hidden')
-		}
-	})
-}
-
-function changeFilterMode(filter) {
-	let mode = filter.textContent
-	const parent = filter.parentElement,
-		previousFilter = parent.querySelector('.filter-dialog-active')
-	previousFilter.classList.remove('filter-dialog-active')
-	filter.classList.add('filter-dialog-active')
-	mode = mode.toUpperCase()
-	$searchContent.setAttribute('data-filter-by', mode)
-	$filter.classList.add('hidden')
+	if (hiddenCounter === $coursesName.length) {
+		$searchNotFound.classList.remove('hidden')
+	} else {
+		$searchNotFound.classList.add('hidden')
+	}
 }
 
 function changeNavBG() {
@@ -86,22 +40,11 @@ function changeNavBG() {
 	}
 }
 
-d.addEventListener('click', (e) => {
-	if (e.target.matches('#search-button')) {
-		toggleSearchDialog(e.target)
-	}
-	if (e.target.matches('#filter-button')) {
-		toggleFilterItems()
-	}
-	if (e.target.matches('.filter-dialog span')) {
-		changeFilterMode(e.target)
-	}
-})
 d.addEventListener('scroll', (e) => {
 	changeNavBG()
 })
 d.addEventListener('keyup', (e) => {
 	if (e.target.matches('.search-dialog input')) {
-		searchItem(e.target)
+		searchItem(e.target.value)
 	}
 })
